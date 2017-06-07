7fa1c87207c2f4ee43c600cf7ef51e9e3d181f32
Trevor <trevor.gallant6@gmail.com>
2017-06-07 11:11:57 -0300

    changes email verification url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'enigmatic-taiga-18444.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
@@ -85,7 +85,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    :domain         => 'enigmatic-taiga-18444.herokuapp.com',
     :enable_starttls_auto => true
   }
 
